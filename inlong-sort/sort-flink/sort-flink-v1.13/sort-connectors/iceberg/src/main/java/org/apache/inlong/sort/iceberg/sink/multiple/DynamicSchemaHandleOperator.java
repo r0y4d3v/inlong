9a63698613d47a03dbd4aed82ffdac6df1612283
Roy <r0y4d3v@outlook.com>
2024-05-03 17:08:03 +0800

    fix: decimal型（如decimal(5, 2)）用=号会认为不相等

--- inlong-sort/sort-flink/sort-flink-v1.13/sort-connectors/iceberg/src/main/java/org/apache/inlong/sort/iceberg/sink/multiple/DynamicSchemaHandleOperator.java
+++ inlong-sort/sort-flink/sort-flink-v1.13/sort-connectors/iceberg/src/main/java/org/apache/inlong/sort/iceberg/sink/multiple/DynamicSchemaHandleOperator.java
@@ -446,6 +446,7 @@ public class DynamicSchemaHandleOperator extends AbstractStreamOperator<RecordWi
                 output.collect(new StreamRecord<>(recordWithSchema));
             } else {
                 LOG.warn("Table {} schema is different!", tableId);
+                LOG.error("Miaow Error: The latest schema is {}, current is {}", latestSchema, dataSchema);
                 if (SchemaUpdateExceptionPolicy.LOG_WITH_IGNORE == multipleSinkOption
                         .getSchemaUpdatePolicy()) {
                     RecordWithSchema recordWithSchema = queue.poll();
@@ -538,8 +539,12 @@ public class DynamicSchemaHandleOperator extends AbstractStreamOperator<RecordWi
         }
 
         return IntStream.range(0, oldSchemaFields.size())
-                .allMatch(i -> oldSchemaFields.get(i).name().equals(newSchemaFields.get(i).name())
-                        && oldSchemaFields.get(i).type() == newSchemaFields.get(i).type());
+                .allMatch(i -> {
+                            oldSchemaFields.get(i).name(), newSchemaFields.get(i).name());
+                            oldSchemaFields.get(i).type(), newSchemaFields.get(i).type());
+                    return oldSchemaFields.get(i).name().equals(newSchemaFields.get(i).name())
+                            && oldSchemaFields.get(i).type().equals(newSchemaFields.get(i).type());
+                });
     }
 
     private TableIdentifier parseId(JsonNode data) throws IOException {
